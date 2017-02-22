e9539b9454ddcaa99b505f2cafc3370acc0d4481
ZoeLeBlanc <zoe.leblanc@vanderbilt.edu>
2017-02-22 15:51:38 -0600

    updates on search

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 $(window).on('load', ()=>{
+  var list;
   $('#cohort-bios').hide()
   // Click-to-scroll for animated arrow
   $('#about-us').click(function () {
@@ -7,20 +8,19 @@ $(window).on('load', ()=>{
           'slow');
   });
   getItems().then( (response)=>{
-    var list = response;
-    var $items = getItem(list);
+    list = response;
+    let $items = getItem(list);
     console.log($items);
     $('#cohort-bios').html( $items );
     $('#cohort-bios').show();
   });
-  
   function getItem(items) {
     var list_of_people = '';
     $.each(items, (index, person)=>{
         list_of_people += `
           <div class="card col s10 offset-s1 col m4 offset-m1 col l3 offset-l1">
             <div class="card-image waves-effect waves-block waves-light">
-              <img class="activator person-image grayscale grayscale-fade" src="${person.professionalPic}" alt="${person.name}">
+              <img class="activator person-image" src="${person.professionalPic}" alt="${person.name}">
                <span class="card-title activator">${person.name}</i></span>
             </div>
             <div class="card-content">
@@ -35,7 +35,6 @@ $(window).on('load', ()=>{
     });
     return $( list_of_people);
   }
-
   function getItems() {
     return new Promise(function (resolve, reject) {
       $.ajax({
@@ -52,10 +51,42 @@ $(window).on('load', ()=>{
       })
     });
   }
-  // $('#cohort-bios').mouseover( (event)=>{
-  //   $('.grayscale').removeClass('grayscale');
-  // });
-  
+  $('.card').on('hover', (event)=>{
+    console.log("event", event);
+    $(this).find('> .card-image > img.activator').click();
+    }, function() {
+      $(this).find('> .card-reveal > .card-title').click();
+    }
+  );
+  $('#search').keypress( (event)=>{
+    if (event.which == 13){
+      let search = $("#search").val();
+      searchBios(search);
+      $("#search").val("");
+    }
+  });
+  $('#search-button').on("click", (event)=>{
+    let search = $("#search").val();
+    searchBios(search);
+    $("#search").val("");
+  });
+  function searchBios(search_query){
+    var found_bios = $.grep(list, (bio, index)=> {
+          console.log(bio);
+          if (bio.name.includes(search_query)){
+            console.log("bio", bio);
+            return bio
+          }
+          if (bio.aboutMe.includes(search_query)){
+            console.log("bio", bio);
+            return bio
+          }
+    });
+    let $items = getItem(found_bios);
+    console.log($items);
+    $('#cohort-bios').html('');
+    $('#cohort-bios').html( $items );
+  }
 });
 
   
